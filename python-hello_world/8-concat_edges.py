e4ab35c17f67cc8386c32bc1cab964a1d7f03cb8
binbashz <5732@holbertonstudents.com>
2023-05-31 11:23:44 -0700

    concat edges by the index [0:6]

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str + " " + "object-oriented programming with Python"
+str = str[39:66] + str[106:112] + str[:6]
+# accedemos a las palabras que queremos tomar,
+# mediante sus indcies o trozos a imprimir
 print(str)
