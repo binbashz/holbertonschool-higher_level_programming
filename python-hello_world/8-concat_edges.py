4816abe394efdaf0ad9b1b8d324fc02f2d889a3e
binbashz <5732@holbertonstudents.com>
2023-05-31 11:26:54 -0700

    8-concat

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,6 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[106:112] + str[:6]
-# accedemos a las palabras que queremos tomar,
-# mediante sus indcies o trozos a imprimir
 print(str)
