908297924d3cefa4e1fc3ba4984173db11b198d7
binbashz <5732@holbertonstudents.com>
2023-06-11 14:36:16 -0700

    try , except, pass

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0  # Variable to keep track of the number of integers printed
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):  # Check if element is an integer
-                print("{:d}".format(my_list[i]), end=" ")  # Print the integer
-                count += 1  # Increment the count of printed integers
-    except IndexError:
-        pass  # Do nothing if an exception occurs
-    finally:
-        print()  # Print a newline character
-        return count  # Return the count of integers printed
+    integer_count = 0  # Var to keep track of the number of integers printed
+    for i in range(0, x):  # Iterate over the specified range of x values
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            integer_count += 1  # Increment the count of printed integers
+        except (ValueError, TypeError):  # Catch ValueErr or TypeError excep
+            pass  # Skip non-integer elements silently
+    print()  # Print a newline character
+    return integer_count  # Return the count of integers printed
