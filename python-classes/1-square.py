b5d5f96960b2cb7f0f6d4ca43216423493966830
Mauricio Correa <5732@holbertonstudents.com>
2023-06-14 13:31:52 -0300

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """ protected data  members """
+    __size = None
+   """ constructor """
+    def _init_(self,__size):
+        self.__size = size
+        
+
+
