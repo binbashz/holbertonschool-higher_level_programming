c91e5c27db38b89c39b64095243c9476bad77b14
binbashz <5732@holbertonstudents.com>
2023-06-15 15:46:17 -0300

    fix param, and arg

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 class Square:
-    """ protected data  members """
-    __size = None
-    """ constructor """
-    def __init__(self, __size):
-        self.__size = __size
+    """
+    class Square defined (I am a comment inside a pair of three double quotes)
+    """
+    def __init__(self, size):
+        """
+        method __init__ is to initialize the attributes of the object we create.
+        """
+        self.__size = size
+        
+    """
+     protected data  members whit double undersocer
+    """
