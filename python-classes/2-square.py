0cd6662b00df67aee950903cf5bf1e2b41a10bfd
binbashz <5732@holbertonstudents.com>
2023-06-15 16:09:51 -0700

    pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
-'''Write a class Square that defines a square by: (based on 1-square.py)'''
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
 
 
 class Square:
+    """defines Square """
 
-    '''Defines square'''
     def __init__(self, size=0):
+
         if type(size) != int:
             raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
