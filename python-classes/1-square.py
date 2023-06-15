a1ee57a6ccb6fe23c819648cfba41fa23c1e76db
binbashz <5732@holbertonstudents.com>
2023-06-15 12:41:20 -0700

    rm and new file

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,19 +2,17 @@
 
 class Square:
     """
-<<<<<<< HEAD
     This is the Square class that defines a square.
 
     Private instance attribute:
         - __size: represents the size of the square
-=======
-    class Square defined
->>>>>>> e3b1a99e21e633b88336b94e06303243bfd31023
     """
 
     def __init__(self, size):
-<<<<<<< HEAD
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
-=======
-        self.__size = size
->>>>>>> e3b1a99e21e633b88336b94e06303243bfd31023
