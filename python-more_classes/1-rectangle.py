3b1b14a027dbd3ef251ecd307087d50c714ee562
binbashz <5732@holbertonstudents.com>
2023-06-17 16:42:28 -0700

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-"""
-Real definition of a rectangle
-"""
+""" Real definition of a rectangle """
 
 
 class Rectangle:
     def __init__(self, width=0, height=0):
+        self.__width = 0
+        self.__height = 0
         self.width = width
         self.height = height
 
