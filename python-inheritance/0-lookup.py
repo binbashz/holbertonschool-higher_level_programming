2cf9ebff34352f3e28e28396f10ca7bf4d264584
binbashz <5732@holbertonstudents.com>
2023-06-21 09:56:50 -0700

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" task 0 inheritance """
+
+
+def lookup(obj):
+    """ Write a function that returns the list
+of available attributes and methods of an object """
+    return(dir(obj))
