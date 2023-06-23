9b72c307a9e3071030dacca58fef9c16087edb65
binbashz <5732@holbertonstudents.com>
2023-06-23 06:09:46 -0700

    Exact same object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
-¨¨¨ task 02 ¨¨¨
+"""
+task 02
+"""
 
 
 def is_same_class(obj, a_class):
-    ¨¨¨
-    true if is a instance
-    ¨¨¨¨
+    """
+  true if is a instance
+    """
     return type(obj) is a_class
