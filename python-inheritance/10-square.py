e3ab3b12f6fef6ff2fa48bd2a0f219f59dd2af3c
binbashz <5732@holbertonstudents.com>
2023-06-24 10:01:16 -0700

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python3
-"""Defines a Square class that inherits from Rectangle"""
-
-
+#!/usr/bin/python3
+""" Task 10. Square # 1
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square, inherits from Rectangle"""
-
+    """ Square class that inherits from Rectangle
+    """
     def __init__(self, size):
-        """Initializes a square instance"""
+        """ Instantiation
+        """
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
-
-    def __str__(self):
-        """Returns the square description"""
-        return "[Square] {}/{}".format(self.__width, self.__height)
