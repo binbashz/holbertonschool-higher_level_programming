e0bdbf34741dfdfecf64c16b4d4b49ba6e6291bb
binbashz <5732@holbertonstudents.com>
2023-06-24 16:57:21 -0700

    correct spelling in comment

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,7 +6,7 @@
 
 def read_file(filename=""):
     """
-    read the text file and use witg
+    read the text file and use with
     """
     with open(filename, encoding="utf-8") as my_file_to_open:
         print(my_file_to_open.read(), end='')
