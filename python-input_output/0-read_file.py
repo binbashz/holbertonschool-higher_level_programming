512198f9174bbb15d7716585575a52b740b7a0ed
binbashz <5732@holbertonstudents.com>
2023-06-24 16:51:08 -0700

    open file utf and with

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" task 0 read file """
+
+
+def read_file(filename="")
+
+
+"""
+read the text  file and use with
+
+"""
+with open(filename, encoding="utf-8") as my_file_to_open:
+    print(my_file_to_open.read(), end='')
