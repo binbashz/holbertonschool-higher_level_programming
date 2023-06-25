7c543978d58d5fcc40abe0eb46e051caacfac3c9
binbashz <5732@holbertonstudents.com>
2023-06-25 14:38:32 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-
-""" 7. Load, add, save
+"""Task 7. Load, add, save
 Add all arguments to a Python list, and then save them to a file
 """
-
 import sys
-from pathlib import Path
-
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-data = {}
 
-if Path(filename).is_file():
-    data = load_from_json_file(filename)
+fileName = "add_item.json"
 
-my_list = data.get('my_list', [])
-my_list.extend(sys.argv[1:])
+if os.path.isfile(fileName):
+    new_list = load_from_json_file(fileName)
+else:
+    new_list = []
 
-data['my_list'] = my_list
-save_to_json_file(data, filename)
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
+
+save_to_json_file(new_list, fileName)
