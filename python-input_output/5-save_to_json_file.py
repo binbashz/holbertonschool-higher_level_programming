f1c461acbbcff1b1f3c23c0d560b5c9f844a4c16
binbashz <5732@holbertonstudents.com>
2023-06-25 14:15:52 -0700

    w

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,7 +6,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, 'w', encoding="utf-8") as file_json:
+    with open(filename, 'W', encoding="utf-8") as file_json:
         """ The json.dump() function is used to serialize the
             object my_obj and write the JSON representation to the file
             The json.dump() function converts the object into a JSON string
