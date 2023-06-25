c2b947c90e565ae2f753bd4ff5b75ada69a548ea
binbashz <5732@holbertonstudents.com>
2023-06-25 14:12:03 -0700

    add utf-8

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,10 +6,10 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as file:
+    with open(filename, 'w', encoding="utf-8") as file_json:
         """ The json.dump() function is used to serialize the
             object my_obj and write the JSON representation to the file
             The json.dump() function converts the object into a JSON string
             representation and writes it to the file.
         """
-        json.dump(my_obj, file)
+        json.dump(my_obj, file_json)
